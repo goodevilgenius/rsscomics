20e5e8a40ffb43c5edf76b0a6ee4d60985cdf1f7
Dan Jones <good.evil.genius@gmail.com>
2013-09-24 11:18:16 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+$id = $_GET['id'];
+$mem = new Memcache;
+$mem->connect('localhost', 11211);
+
+$feed = "http://feeds.feedburner.com/uclick/$id";
+$rss_xml = $mem->get($feed);
+if (empty($rss_xml)) {
+  $rss_xml = file_get_contents($feed);
+  foreach($http_response_header as $header) {
+    if (strpos($header, "Expires: ") === 0) $exp = ltrim($header, "Expires: ");
+  }
+  
+  if (!empty($exp)) {
+    header("Expires: $exp");
+    $mem->set($feed, $rss_xml, 0, time() - strtotime($exp));
+  }
+}
+
+$rss = new simplexmlelement($rss_xml);
+//$rss_heads = $http_response_header;
+$title = preg_replace('@GoComics.com - (.+) by .+@', '\1', $rss->channel->title);
+
+foreach($rss->channel->item as $i) {
+  $html = $mem->get($i->link);
+  if (empty($html)) {
+    $html = file_get_contents($i->link);
+    $mem->set($i->link, $html);
+  }
+
+  $i->title = "$title: " . $i->title;
+  if (preg_match('@<img[^>]+class="strip"[^>]+src="([^"]+)"[^>]+>@',$html,$m)) {
+    $i->description = $m[0];
+  }
+}
+
+header("Content-type: application/rss+xml");
+echo $rss->saveXML();
